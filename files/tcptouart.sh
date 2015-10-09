b20e36ddb7cf7bc01b9ffe25a84ee71092449915
RuanJG <pi@raspberrypi.(none)>
2015-10-09 09:33:20 +0000

    Ruan: add ip check in script

--- files/tcptouart.sh
+++ files/tcptouart.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
-uart_name='/dev/ttyUSB0'
+uart_name=$(find /dev -name ttyUSB*)
 #interface
 net=wlan0
+nip=192.168.1.108
 while [ 0 ]
 do
 	ip=$(ifconfig $net  | grep 'inet addr')
 	ip=${ip#*'inet addr:'}
 	ip=${ip%' Bcast:'*}
-	if [ "$ip"'x' = 'x' ];then
-		echo can not find ip in br-lan
+	ip=${ip%' '}
+	if [ -z $ip ];then
+		echo no ip
 	else
-        	echo "start tcpuart server, in $ip"
-        	/bin/tcptouart $ip 6666 $uart_name 57600
+		echo $ip
+		if [ "$ip" = "$nip" ];then
+        		echo "start tcpuart server, in $ip"
+        		/bin/tcptouart $ip 6666 $uart_name 57600
+		else
+			echo can not connect ip $nip
+		fi
 	fi
 	sleep 3
 done
