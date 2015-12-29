044457093fb65f450ed380605f194ab52d66d63c
RuanJG <pi@example.com>
2015-12-29 04:44:16 +0000

    Ruan: fix some wrong define

--- o2o_service.h
+++ o2o_service.h
@@ -1,4 +1,4 @@
-int o2o_service_main(int argc, char  ** argv);
+void* o2o_service_main(void *arg);
 void call_o2o_service_quit();
 void call_update_o2o_online_status(int status);
 void call_update_o2o_4G_ip();
