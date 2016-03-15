9d1bd1b817c19afe1ced0d635c7869f09d4f900d
RuanJG <ruan.mails@gmailn.com>
2016-03-15 08:56:03 +0000

    Ruan: o2oservice.sh add name config

--- files/o2oservice.sh
+++ files/o2oservice.sh
@@ -1,7 +1,8 @@
 #!/bin/sh 
-net='wwan0'
+net='wlan0'
 date=''
 time=''
+copter_name='4g_copter'
 while [ 0 ]
 do
 	ip=$(ifconfig $net | grep 'inet addr')
@@ -15,7 +16,7 @@ do
         	echo "start o2oserver, in $ip"
 		date=$(date -d today +"%Y-%m-%d")
 		time=$(date -d today +"%H:%M:%S")
-		curl "http://www.o2oc.cn/UAV/RuanJG/123?id=4G_Camera&ip=$ip&iol=1&mac=10:22:33:44:55:66&olt=$date%20$time"
+		curl "http://www.o2oc.cn/UAV/RuanJG/123?id=$copter_name&ip=$ip&iol=1&mac=10:22:33:44:55:66&olt=$date%20$time"
 	fi
 	sleep 3
 done
